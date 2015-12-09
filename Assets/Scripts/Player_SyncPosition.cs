e6c4d86a5ce6ba6093891c60b41299c1ada6c6ca
Sasha Nicolas <sasha@tecgraf.puc-rio.br>
2015-12-09 20:13:43 -0200

    Modificando Player do Unity para Rede

---
+++ Assets/Scripts/Player_SyncPosition.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player_SyncPosition : NetworkBehaviour {
+
+    [SyncVar]
+    private Vector3 syncPos;
+    [SerializeField]
+    Transform myTransform;
+    [SerializeField]
+    float lerpRate = 15;
+
+    Vector3 lastPos;
+    float threshold = 0.5f;
+    
+
+	void FixedUpdate () {
+        TransmitPosition();
+        LerpPosition();
+	}
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdprovidePositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if (isLocalPlayer && Vector3.Distance(myTransform.position, lastPos) > threshold)
+        {
+            CmdprovidePositionToServer(myTransform.position);
+            lastPos = myTransform.position;
+        }
+            
+    }
+
+}
---
+++ Assets/Scripts/Player_SyncRotation.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player_SyncRotation : NetworkBehaviour {
+
+    [SyncVar]
+    Quaternion syncPlayerRotation;
+    [SyncVar]
+    Quaternion syncCamRotation;
+
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Transform camTransform;
+
+    [SerializeField]
+    float lerpRate = 15;
+
+    Quaternion lastPlayerRot;
+    Quaternion lastCamRot;
+    float threshold = 5;
+
+    void Update()
+    {
+        LerpRotation();
+    }
+
+	void FixedUpdate () {
+        TransmitRotations();        
+	}
+
+    void LerpRotation()
+    {
+        playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, syncPlayerRotation, Time.deltaTime * lerpRate);
+        camTransform.rotation = Quaternion.Lerp(camTransform.rotation, syncCamRotation, Time.deltaTime * lerpRate);
+    }
+
+    [Command]
+    void CmdProvideRotationsToServer(Quaternion playerRot, Quaternion camRot)
+    {
+        syncPlayerRotation = playerRot;
+        syncCamRotation = camRot;
+    }
+
+    [Client]
+    void TransmitRotations()
+    {
+        if (isLocalPlayer)
+        {
+            if ((Quaternion.Angle(playerTransform.rotation, lastPlayerRot) > threshold || Quaternion.Angle(camTransform.rotation, lastCamRot) > threshold))
+            {
+                CmdProvideRotationsToServer(playerTransform.rotation, camTransform.rotation);
+                lastPlayerRot = playerTransform.rotation;
+                lastCamRot = camTransform.rotation;
+            }            
+        }
+    }
+}
